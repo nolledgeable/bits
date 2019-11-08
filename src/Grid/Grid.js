30109f899442854567dc3bb269e3df08f4346058
nolle <nolle@iconis.ch>
2019-11-08 23:54:22 +0100

    Add config files

---
+++ src/Grid/Grid.js
@@ -0,0 +1,20 @@
+// import React from 'react';
+// import PropTypes from 'prop-types';
+//
+//
+// const Grid = ({
+//   element = 'div',
+//
+// }) => (
+//   <{element}>
+//   </{element}>
+// );
+//
+//
+// Grid.propTypes = {
+//   element: PropTypes.string,
+//
+// };
+//
+//
+// export default Grid;
